3b891949e92caafd70610cc9bac41d9c4719f968
Akash <dalbhanjanakash1@gmail.com>
2026-03-26 20:27:11 +0530

    make changes in prisma connection string

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const PORT = process.env.PORT || 3000;
-
+console.log(process.env.DATABASE_URL);
 app.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT}`);
+    console.log(`Serve is running on ${PORT}`);
 })
